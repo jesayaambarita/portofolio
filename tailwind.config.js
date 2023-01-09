84828a88eb3b95384c656be0106ccae3ada95a66
Jesaya Ambarita <jesayaambarita12355@gmail.com>
2023-01-09 17:27:16 +0700

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      content: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#0d9488',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screen: {
+        '2xl': '1230px',
+      }
+    },
+  },
+  plugins: [],
+}
