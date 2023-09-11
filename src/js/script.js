9faf43332a6b5d7eabfb5005ea7e589ce9ec3671
Jesaya Sohasuhatan Ambarita <73833738+jesayaambarita@users.noreply.github.com>
2023-09-11 09:39:34 +0700

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -53,4 +53,17 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
   }
 
   // fungsi initialize untuk mempersiapkan peta
+
+
+<!--Start of Tawk.to Script-->
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/64fe7d000f2b18434fd7c410/1ha11gk2v';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+<!--End of Tawk.to Script-->
   
